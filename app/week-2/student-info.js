2cf4d782d5f8cc7d6f0ac6c4d70c07fec4243524
kkhan4724 <152225776+kkhan4724@users.noreply.github.com>
2024-09-19 13:04:26 -0600

    Added GitHub repo link
    https://github.com/kkhan4724/cprg306-assignments.git

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,7 +2,7 @@ export default function StudentInfo() {
     return (
         <header>
             <h1>Name: Khush Khan</h1>
-            <p>GitHub repo: </p>
+            <p>GitHub repo:https://github.com/kkhan4724/cprg306-assignments.git </p>
         </header>
     );
 }
