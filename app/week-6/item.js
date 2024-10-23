7f2e7a2040f87f8a5b40c7c18684be5f41830f98
kkhan4724 <152225776+kkhan4724@users.noreply.github.com>
2024-10-22 19:07:57 -0600

    Week 6
    Refactored ItemList component: removed hardcoded items, integrated useState for sorting, and loaded items from JSON file. Added sorting functionality with buttons for 'name' and 'category' with conditional styling."

---
+++ app/week-6/item.js
@@ -0,0 +1,12 @@
+export default function Item({name, quantity, category}) {
+    return(
+        <li className="p-2 bg-gray-200 border rounded mb-1">
+            
+            <p className="font-bold">{name}</p>
+            <p className="text-sm">Quantity: {quantity}</p>
+            <p className="text-sm">Category: {category}</p>
+
+        </li>
+
+    );
+}
