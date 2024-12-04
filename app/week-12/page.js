4d2883c111a60a6a415c8cb52c2edcf884efa0da
kkhan4724 <152225776+kkhan4724@users.noreply.github.com>
2024-12-03 20:27:20 -0700

    Added pages

---
+++ app/week-12/page.js
@@ -0,0 +1,10 @@
+
+export default function Page() {
+    return (
+        <main className="p-4 bg-gray-50 min-h-screen">
+          <h1 className="text-3xl font-bold mb-6 text-center">Week 12</h1>
+          
+        </main>
+    );
+
+}
